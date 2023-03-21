54611f440182e159ec11801981a4863ab141fef7
Joamoi <joni.aitto.oja@hotmail.com>
2023-03-21 14:04:01 +0200

    Audio mixer created for music and effects volumes

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject mainMenuButtons;
     public GameObject settingsButtons;
+
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider effectsSlider;
+    //public AudioSource testEffectSound;
+
+    void Start()
+    {
+        if (!(PlayerPrefs.GetFloat("musicVol") == 0))
+        {
+            audioMixer.SetFloat("musicVolume", Mathf.Log10(PlayerPrefs.GetFloat("musicVol")) * 20);
+            musicSlider.value = (PlayerPrefs.GetFloat("musicVol"));
+        }
+
+        if (!(PlayerPrefs.GetFloat("effectsVol") == 0))
+        {
+            audioMixer.SetFloat("effectsVolume", Mathf.Log10(PlayerPrefs.GetFloat("effectsVol")) * 20);
+            effectsSlider.value = (PlayerPrefs.GetFloat("effectsVol"));
+        }
+    }
 
     public void PlayGame()
     {
@@ -32,14 +54,22 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void SetMusicVolume()
-    {
+    // sliders in settings control the volumes in audiomixer
 
+    public void SetMusicVolume(float volume)
+    {
+        // slider would be logaritmic without the fix
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVol", volume);
     }
 
-    public void SetEffectsVolume()
+    public void SetEffectsVolume(float volume)
     {
+        // slider would be logaritmic without the fix
+        audioMixer.SetFloat("effectsVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("effectsVol", volume);
 
+        //testEffectSound.Play(0);
     }
 
     public void Back()
